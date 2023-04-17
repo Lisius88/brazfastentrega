60cce1ddd75e99474b483665fd4d00bb96a10401
Lisius88 <104981367+Lisius88@users.noreply.github.com>
2023-04-17 08:46:20 +0000

    Deploying to gh-pages from @ Lisius88/brazfastentrega@299671fa21e1db6a94e27fbe15592c995a504cf7 🚀

---
+++ contacts.aa6e99ba.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),c=document.querySelector(".modal"),o=document.querySelector(".close");console.log(e),e.addEventListener("click",(function(){c.style.display="block"})),o.addEventListener("click",(function(){c.style.display="none"}));
+//# sourceMappingURL=contacts.aa6e99ba.js.map
---
+++ contacts.b46274d4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger"),c=document.querySelector(".modal"),n=document.querySelector(".close");console.log(e),e.addEventListener("click",(function(){c.style.display="block"})),n.addEventListener("click",(function(){c.style.display="none"}))}();
+//# sourceMappingURL=contacts.b46274d4.js.map
